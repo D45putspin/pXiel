cb78f5940afac4e07c4f85602d9dc5b670f2984e
D45putspin <t14g0@protonmail.com>
2025-08-01 09:03:46 +0100

    f

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,22 +1,10 @@
 'use client'
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import Nav from './Nav';
 import Section from './Section';
-import { handleWalletError, handleWalletInfo } from "../lib/js/main";
-import { updateCurrentCounter } from "../lib/js/node";
-import WalletUtilService from '../lib/wallet-util-service';
 
 const Main = () => {
-    useEffect(() => {
-        async function setup() {
-            const XianWalletService = WalletUtilService.getInstance();
-            const info = await XianWalletService.XianWalletUtils.requestWalletInfo().catch(handleWalletError)
-            updateCurrentCounter()
-            handleWalletInfo(info);
-        }
-        setup()
-    }, []);
     return (
         <>
             <Nav />
